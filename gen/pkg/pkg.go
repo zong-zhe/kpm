704c1833aecb6e054cf4189afbaabb6021390a20
zong-zhe <zongzhe1024@163.com>
2023-03-16 21:13:02 +0800

    tmp

--- gen/pkg/pkg.go
+++ gen/pkg/pkg.go
@@ -5,3 +5,14 @@ import "strings"
 func (dep *Dependency) Eq(other *Dependency) bool {
 	return strings.EqualFold(dep.Name, other.Name)
 }
+
+func (dep *Dependency) Download() (string, error) {
+	if d, ok := dep.GetDependency().(*Dependency_Git); ok {
+		return d.Git.Download()
+	}
+	return "", nil
+}
+
+func (dep *GitDependency) Download() (string, error) {
+	
+}
